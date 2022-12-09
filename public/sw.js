8297d7464e68c92d110ef16a8d82b85599d421b3
mohsen-pashootan <tahmasebpour.mohsen@gmail.com>
2022-12-10 00:25:50 +0330

    cleanup old caches

--- public/sw.js
+++ public/sw.js
@@ -22,6 +22,18 @@ self.addEventListener("install", function (event) {
 
 self.addEventListener("activate", function (event) {
   console.log("[Service Worker] Activating Service Worker ...", event);
+  event.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== "static-v2" && key !== "dynamic") {
+            console.log("[Service Worker] Removing old cache.", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
   return self.clients.claim();
 });
 
