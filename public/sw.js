f629377383926a339cb251b65a8d26585400913d
mohsen-pashootan <tahmasebpour.mohsen@gmail.com>
2022-12-09 23:40:51 +0330

    add static caching and dynamic caching on the fly

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,23 @@
 self.addEventListener("install", function (event) {
   console.log("[Service Worker] Installing Service Worker ...", event);
+  event.waitUntil(
+    caches.open("static").then(function (cache) {
+      console.log("[service worker] precaching App Shell");
+      cache.addAll([
+        "/", // must add '/', for we cache urls not files. we cache requests
+        "/index.html",
+        "/src/js/app.js",
+        "/src/js/app.js",
+        "/src/js/material.min.js",
+        "/src/css/app.css",
+        "/src/css/feed.css",
+        "/src/images/main-image.jpg",
+        "https://fonts.googleapis.com/css?family=Roboto:400,700", // be aware  that third party pre-caching service must set cors header to not throw us error
+        "https://fonts.googleapis.com/icon?family=Material+Icons",
+        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
+      ]);
+    })
+  );
 });
 
 self.addEventListener("activate", function (event) {
@@ -8,6 +26,20 @@ self.addEventListener("activate", function (event) {
 });
 
 self.addEventListener("fetch", function (event) {
-  console.log("[Service Worker] Fetching something ....", event);
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request)
+          .then(function (res) {
+            return caches.open("dynamic").then(function (cache) {
+              cache.put(event.request.url, res.clone());
+              return res;
+            });
+          })
+          .catch(function (err) {});
+      }
+    })
+  );
 });
