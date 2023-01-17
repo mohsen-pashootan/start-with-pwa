31f4cdce2452a30c5d5aef7b562adba2b8856d8c
mohsen-pashootan <tahmasebpour.mohsen@gmail.com>
2023-01-17 20:54:00 +0330

    comment first strategy

--- public/sw.js
+++ public/sw.js
@@ -59,29 +59,29 @@ self.addEventListener("activate", function (event) {
 });
 
 // #1 cache with network fallback
-self.addEventListener("fetch", function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      if (response) {
-        return response;
-      } else {
-        return fetch(event.request)
-          .then(function (res) {
-            return caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
-              // trimCache(CACHE_DYNAMIC_NAME, 3);
-              cache.put(event.request.url, res.clone()); //dynamic caching
-              return res;
-            });
-          })
-          .catch(function (err) {
-            return caches.open(CACHE_STATIC_NAME).then(function (cache) {
-              return cache.match("/offline.html");
-            });
-          });
-      }
-    })
-  );
-});
+// self.addEventListener("fetch", function (event) {
+//   event.respondWith(
+//     caches.match(event.request).then(function (response) {
+//       if (response) {
+//         return response;
+//       } else {
+//         return fetch(event.request)
+//           .then(function (res) {
+//             return caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
+//               // trimCache(CACHE_DYNAMIC_NAME, 3);
+//               cache.put(event.request.url, res.clone()); //dynamic caching
+//               return res;
+//             });
+//           })
+//           .catch(function (err) {
+//             return caches.open(CACHE_STATIC_NAME).then(function (cache) {
+//               return cache.match("/offline.html");
+//             });
+//           });
+//       }
+//     })
+//   );
+// });
 
 // #2 cache-only
 // self.addEventListener("fetch", function (event) {
