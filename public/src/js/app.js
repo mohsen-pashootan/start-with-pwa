0daddf23d2141438a6a0d1d8245804c559d36da1
mohsen-pashootan <tahmasebpour.mohsen@gmail.com>
2023-01-26 16:00:02 +0330

    adding front-side of web push notif

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,4 +1,7 @@
 var deferredPrompt;
+var enableNotificationsButtons = document.querySelectorAll(
+  ".enable-notifications"
+);
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/sw.js").then(function () {
@@ -12,3 +15,112 @@ window.addEventListener("beforeinstallprompt", function (event) {
   deferredPrompt = event;
   return false;
 });
+
+function displayConfirmNotification() {
+  if ("serviceWorker" in navigator) {
+    var options = {
+      body: "You successfully subscribed to our Notification service!",
+      icon: "/src/images/icons/app-icon-96x96.png",
+      image: "/src/images/sf-boat.jpg",
+      dir: "ltr",
+      lang: "en-US", // BCP 47,
+      vibrate: [100, 50, 200],
+      badge: "/src/images/icons/app-icon-96x96.png",
+      tag: "confirm-notification", // good to control order and count of grop of notifs
+      renotify: true, // if true even use the same tag notif will show | false will stop display duplicate notif
+      actions: [
+        {
+          action: "confirm",
+          title: "Okay",
+          icon: "/src/images/icons/app-icon-96x96.png",
+        },
+        {
+          action: "cancel",
+          title: "Cancel",
+          icon: "/src/images/icons/app-icon-96x96.png",
+        },
+      ],
+    };
+
+    navigator.serviceWorker.ready.then(function (swreg) {
+      swreg.showNotification("Successfully subscribed (from SW)!", options);
+    });
+  } else {
+    alert("SW not supported");
+  }
+
+  // // normal notification without service worker
+  // var options = {
+  //   body: "You successfully subscribed to our Notification service!",
+  // };
+  // new Notification("Successfully subscribed!", options);
+}
+
+function configurePushSub() {
+  if (!("serviceWorker" in navigator)) {
+    return;
+  }
+
+  var reg;
+  navigator.serviceWorker.ready
+    .then(function (swreg) {
+      reg = swreg;
+      return swreg.pushManager.getSubscription();
+    })
+    .then(function (sub) {
+      if (sub === null) {
+        // Create a new subscription
+
+        // this vapidPublicKey  will come from backend. a package like web-push
+        var vapidPublicKey =
+          "BKapuZ3XLgt9UZhuEkodCrtnfBo9Smo-w1YXCIH8YidjHOFAU6XHpEnXefbuYslZY9vtlEnOAmU7Mc-kWh4gfmE";
+        var convertedVapidPublicKey = urlBase64ToUint8Array(vapidPublicKey);
+        return reg.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: convertedVapidPublicKey,
+        });
+      } else {
+        // We have a subscription
+      }
+    })
+    .then(function (newSub) {
+      return fetch("https://pwagram-99adf.firebaseio.com/subscriptions.json", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(newSub),
+      });
+    })
+    .then(function (res) {
+      if (res.ok) {
+        displayConfirmNotification();
+      }
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+function askForNotificationPermission() {
+  Notification.requestPermission(function (result) {
+    console.log("User Choice", result);
+    if (result !== "granted") {
+      console.log("No notification permission granted!");
+    } else {
+      configurePushSub();
+      // displayConfirmNotification();
+    }
+  });
+}
+
+if ("Notification" in window) {
+  for (var i = 0; i < enableNotificationsButtons.length; i++) {
+    enableNotificationsButtons[i].style.display = "inline-block";
+    enableNotificationsButtons[i].addEventListener(
+      "click",
+      askForNotificationPermission
+    );
+  }
+}
